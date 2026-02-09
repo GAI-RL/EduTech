c9277efa9cc6096bfb32ec4d5f24de1f0aaf1e74
GAI-RL <airl@kics.edu.pk>
2026-02-09 15:35:22 +0500

    Update websocket.service.ts

--- src/app/services/websocket.service.ts
+++ src/app/services/websocket.service.ts
@@ -17,7 +17,7 @@ export class WebSocketService {
     // const wsUrl = `wss://edullm-backend.onrender.com/ws/chat/${sessionId}?user_id=${encodeURIComponent(userId)}`;
     const wsUrl = `wss:/steam-backend-rycd.onrender.com/ws/chat/${sessionId}?user_id=${encodeURIComponent(userId)}`; // new BE
      // private apiUrl = 'https://edutech-2282.onrender.com';
-     const wsUrl = `wss://edutech-2282.onrender.com/ws/chat/${sessionId}?user_id=${encodeURIComponent(userId)}`; //New URL
+    // const wsUrl = `wss://edutech-2282.onrender.com/ws/chat/${sessionId}?user_id=${encodeURIComponent(userId)}`; //New URL
     // const wsUrl = `wss://homogenous-preobvious-bell.ngrok-free.dev/ws/chat/${sessionId}?user_id=${encodeURIComponent(userId)}`;
     console.log('Connecting to WebSocket:', wsUrl);
     this.socket = new WebSocket(wsUrl);
